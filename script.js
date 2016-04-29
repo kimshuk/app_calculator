c929e5d0c7cbbc5bbb01e26e57a6aadc250370ef
Andrew Kim <jeesoo2002@gmail.com>
2016-04-29 10:28:51 -0700

    v1.0 basic function done

--- script.js
+++ script.js
@@ -1,95 +1,78 @@
+var numArray = ['',''];
 var memory = "0";
 var current = "0";
-var operation = 0;
+var operator = "";
+var index = 0;
 MAXLENGTH = 15;
 
 //function to input numbers on display section
 //LF START
 function display (dig) {
     //check if the user input is too long
-    if (current.length > MAXLENGTH) {
-        current = "Please have digits shorter than 10 digits";      //notify to have input shorter than 10
+    if (numArray[index].length > MAXLENGTH) {
+        numArray[index] = "Please have digits shorter than 15 digits";      //notify to have input shorter than 10
         //check if prev input is 0, if yes, replace it with next input
-    } else if ( (eval(current) == 0)) {     //if input on screen is 0, replace it with next input
-        current = dig;
-    } else {        //if prev input on the screen is not 0, then add to prev
-        current += dig;
+    } else if (numArray[index] == 0) {
+        numArray[index] = dig;
+    } else {
+        numArray[index] += dig;
     }
-    $('#output').html(current);     //display the input on the screen
+    $('#output').html(numArray[index]);     //display the input on the screen
 }
 //LF END
-function fixInput() {
-    current = "" + parseFloat($('#output').html());
-    if (current.indexOf("NaN") != -1) {
-        current = "I don't understand your input";
-    }
-    $("#output").html(current);
-}
+// function fixInput() {
+//     current = "" + parseFloat($('#output').html());
+//     if (current.indexOf("NaN") != -1) {
+//         current = "I don't understand your input";
+//     }
+//     $("#output").html(current);
+// }
 
 //function for C and CE
 function clear() {
-    current = "0";
-    $('#output').html(current);
+    index = 0;
+    numArray[1] = 0;
+    $('#output').html(numArray[index]);
 }
 
 function allClear() {
-    current = "0";
-    memory = "0";
-    operation = 0;
+    index = 0;
+    numArray[0] = 0;
+    numArray[1] = 0;
     $('#output').html(current);
 }
 
 //function to respond operators (+, -, /, *)
 //LF START
 function operate(op) {
-    switch (op) {
-        case (op.indexOf("&times;") >= 0):      //find if user entered is "*"
-            operation = 1;                      //mark it as 1
+    operator = op;
+    $('#output').html(operator);     //display the input operator on the screen
+    index++;
+}
+//LF END
+//function to calculate (press "=")
+function calculate() {
+    var result = 0;
+    switch (operator) {
+        case "*":      //find if user entered is "*"
+            result = parseInt(numArray[0]) * parseInt(numArray[1]);
             break;
-        case (op.indexOf("&divide;") >= 0):     //find if user entered is "/"
-            operation = 2;                      //mark it as 2
+        case "/":     //find if user entered is "/"
+            result = parseInt(numArray[0]) / parseInt(numArray[1]);
             break;
-        case (op.indexOf("&plus;") >= 0):       //find if user entered is "+"
-            operation = 3;                      //mark it as 3
+        case "+":       //find if user entered is "+"
+            result = parseInt(numArray[0]) + parseInt(numArray[1]);
             break;
-        case (op.indexOf("&minus;") >= 0):      //find if user entered is "-"
-            operation = 4;                      //mark it as 4
+        case "-":      //find if user entered is "-"
+            result = parseInt(numArray[0]) - parseInt(numArray[1]);
             break;
         default:
             console.log("what is this?");       //foolproof message: throws an error message on console
             break;
     }
-    memory = current;   //store current value to memory
-    current = "";       //reset current value
-    $('#output').html(current);     //display the input operator on the screen
-}
-//LF END
-//function to calculate (press "=")
-function calculate() {
-    switch (operation) {
-        case 1:
-            current = eval(memory) * eval(current);
-            break;
-        case 2:
-            if (current == 0) {
-                current = "Cannot divide by 0";
-            } else {
-                current = eval(memory) / eval(current);
-            }
-            break;
-        case 3:
-            current = eval(memory) + eval(current);
-            break;
-        case 4:
-            current = eval(memory) - eval(current);
-            break;
-        default:
-            console.log("something wrong");
-            break;
-    }
-    operation = 0;      //after calculation, reset operation to 0
-    memory = "0";       //after calculation, reset num stored in memory to 0
-    current = current + "";     //forces num stored in current to string
+    numArray[0] = 0;
+    numArray[1] = 0;
+    $('#output').html(result);
 }
 
 $(document).ready(function(){
@@ -99,7 +82,10 @@ $(document).ready(function(){
         console.log("btn clicked: ", val);
 
         switch (val) {
-            case ('&minus;' || '&plus;' || '&divide;' || '&times;'):
+            case "-":
+            case "/":
+            case "*":
+            case "+":
                 operate(val);
                 break;
             case "=":
@@ -113,7 +99,6 @@ $(document).ready(function(){
                 break;
             default:
                 display(val);
-                // fixInput();
                 break;
         }
     });
