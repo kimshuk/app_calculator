1f34fd239ce97028aaa17441ad125bb2031bc989
Andrew Kim <jeesoo2002@gmail.com>
2016-05-02 09:46:56 -0700

    calculator v1 - Andrew Kim

--- script.js
+++ script.js
@@ -1,77 +1,94 @@
-var numArray = ['',''];
-var memory = "0";
-var current = "0";
-var operator = "";
+var numArray = [];
+var numObj = {};
+var multiDigit = "";
+// var operator = "";
 var index = 0;
 MAXLENGTH = 15;
 
 //function to input numbers on display section
-//LF START
 function display (dig) {
     //check if the user input is too long
-    if (numArray[index].length > MAXLENGTH) {
-        numArray[index] = "Please have digits shorter than 15 digits";      //notify to have input shorter than 10
-        //check if prev input is 0, if yes, replace it with next input
-    } else if (numArray[index] == 0) {
-        numArray[index] = dig;
+    if (multiDigit.length >= MAXLENGTH) {
+        multiDigit = "Please put less than 15 digits";
+    } else if ( multiDigit == "0") {
+        multiDigit = dig;
     } else {
-        numArray[index] += dig;
+        multiDigit += dig;
     }
-    $('#output').html(numArray[index]);     //display the input on the screen
+    $('#output').html(multiDigit);     //display the input on the screen
 }
-//LF END
-// function fixInput() {
-//     current = "" + parseFloat($('#output').html());
-//     if (current.indexOf("NaN") != -1) {
-//         current = "I don't understand your input";
-//     }
-//     $("#output").html(current);
-// }
 
 //function for C and CE
 function clear() {
-    index = 0;
-    numArray[1] = 0;
-    $('#output').html(numArray[index]);
+    multiDigit = 0;
+    numObj = {};
+    $('#output').html(multiDigit);
 }
 
 function allClear() {
-    index = 0;
-    numArray[0] = 0;
-    numArray[1] = 0;
-    $('#output').html(current);
+    multiDigit = 0;
+    numArray = [];
+    $('#output').html(multiDigit);
 }
 
-//function to respond operators (+, -, /, *)
-//LF START
-function operate(op) {
-    operator = op;
-    $('#output').html(operator);     //display the input operator on the screen
-    index++;
+//function for decimal point
+function dec() {
+    if (multiDigit.length == 0) {
+        multiDigit = "0.";
+    } else {
+        if (multiDigit.indexOf(".") == -1) {
+            multiDigit += ".";
+        }
+    }
+    $('#output').html(multiDigit);
 }
-//LF END
+
+
+//function to respond operators (+, -, /, *)
+function operate(op) {
+    numObj = {      //put value into object's key
+        value: parseFloat(multiDigit),
+        type: op
+    };
+    numArray.push(numObj);
+    multiDigit = "";
+    $('#output').html(operator);     //display the input operator on the screen
+}
 //function to calculate (press "=")
 function calculate() {
     var result = 0;
-    switch (operator) {
-        case "*":      //find if user entered is "*"
-            result = parseInt(numArray[0]) * parseInt(numArray[1]);
-            break;
-        case "/":     //find if user entered is "/"
-            result = parseInt(numArray[0]) / parseInt(numArray[1]);
-            break;
-        case "+":       //find if user entered is "+"
-            result = parseInt(numArray[0]) + parseInt(numArray[1]);
-            break;
-        case "-":      //find if user entered is "-"
-            result = parseInt(numArray[0]) - parseInt(numArray[1]);
-            break;
-        default:
-            console.log("what is this?");       //foolproof message: throws an error message on console
-            break;
+
+    numObj = {
+        value: parseFloat(multiDigit)
+    };
+    numArray.push(numObj);
+
+    for(var i=0; i<numArray.length - 1; i++) {
+        switch (numArray[i].type) {
+            case "*":      //find if user entered is "*"
+                result = parseFloat(numArray[i].value) * parseFloat(numArray[i + 1].value);
+                break;
+            case "/":     //find if user entered is "/"
+                if (numArray[i+1].value == 0) {
+                    result = "error";
+                } else {
+                    result = parseFloat(numArray[i].value) / parseFloat(numArray[i + 1].value);
+                }
+                break;
+            case "+":       //find if user entered is "+"
+                result = parseFloat(numArray[i].value) + parseFloat(numArray[i + 1].value);
+                break;
+            case "-":      //find if user entered is "-"
+                result = parseFloat(numArray[i].value) - parseFloat(numArray[i + 1].value);
+                break;
+            default:
+                console.log("what is this?");       //foolproof message: throws an error message on console
+                break;
+        }
+        numArray[i+1].value = result;
     }
-    numArray[0] = 0;
-    numArray[1] = 0;
+    multiDigit = numArray[numArray.length - 1].value;
+    numArray = [];
     $('#output').html(result);
 }
 
@@ -81,25 +98,18 @@ $(document).ready(function(){
         var val = $(this).text();
         console.log("btn clicked: ", val);
 
-        switch (val) {
-            case "-":
-            case "/":
-            case "*":
-            case "+":
-                operate(val);
-                break;
-            case "=":
-                calculate();
-                break;
-            case 'C':
-                clear();
-                break;
-            case 'CE':
-                allClear();
-                break;
-            default:
-                display(val);
-                break;
+        if ((val == "-") || (val == "/") || (val == "+") || (val == "*")) {
+            operate(val);
+        } else if (val == "=") {
+            calculate();
+        } else if (val == "C") {
+            clear();
+        } else if (val == "CE") {
+            allClear();
+        } else  if (val != NaN) {
+            display(val);
+        } else if (val = ".") {
+            dec();
         }
     });
 });
